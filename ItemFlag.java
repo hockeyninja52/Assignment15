6fe7fde5bc05bf943347d5406c106cbbcd670b96
hockeyninja52 <concordiacardinals23@gmail.com>
2024-12-08 17:38:45 -0500

    Create ItemFlag.java

---
+++ ItemFlag.java
@@ -0,0 +1,19 @@
+package wk15d1;
+
+//Q1 what are the values?
+//A1 integer array
+
+//Q2 why do we use it?
+//A2 Easier to read and has less errors
+
+//Q3 any other benefits?
+//A3 faster
+
+//Q4 why do we use enum
+//A4 whe we need some constants in the code
+
+public enum ItemFlag {
+	empty_since_start,
+	empty_after_removal,
+	normal
+}
